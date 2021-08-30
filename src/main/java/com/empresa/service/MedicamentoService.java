668b40083bf967359ad26f1f1ce6cbfd5d11c2ba
Piero <pierodaniel2000@gmail.com>
2021-08-30 01:41:42 -0500

    Actividad_1

---
+++ src/main/java/com/empresa/service/MedicamentoService.java
@@ -0,0 +1,11 @@
+package com.empresa.service;
+
+import java.util.List;
+
+import com.empresa.entity.Medicamento;
+
+public interface MedicamentoService {
+
+	public List<Medicamento> listaMedicamento();
+	public Medicamento insertaMedicamento(Medicamento med);
+	}
